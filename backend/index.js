07e13dd0fd836d2b0f3281e646a3bb6938baab3a
Ivie <ivieosimen1@gmail.com>
2026-09-14 10:21:14 +0100

    I made a few adjustments to the index.js file

--- backend/index.js
+++ backend/index.js
@@ -4,28 +4,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-
-const movies = require("./models/movies.js");
-//const movieRoutes = require("./routes/movieRoutes.js");
-
 //Initialize the app
 const app = express();
-
-/*app.use("/api/movies", movies);*/
 
 //Set up middleware
 app.use(cors());
 app.use(express.json());
 
-
-//Start the server
-const PORT = process.env.PORT || 3000;
-
+//Define the routes
 app.get("/", (req, res) => {
   res.send("Welcome to Cinevo");
 });
 
+const moviesRoutes = require("./routes/moviesRoutes.js");
+app.use("/api/movies", moviesRoutes);
 
+//Start the server
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- backend/models/bookings.js
+++ backend/models/bookings.js
@@ -22,6 +22,63 @@ function create({ showtimeId, seatIds, customerName, customerEmail, total }) {
   };
   bookings.push(booking);
   return booking;
-}
+};
+
+function update(id, updates) { showtimeId, seatIds, customerName, customerEmail, total, status } = updates;
+  const index = bookings.findIndex((b) => b.id === Number(id));
+  if (index === -1) return null;
+  bookings[index] = { ...bookings[index], ...updates, id: Number(id) };
+  return bookings[index];
+
 
 module.exports = { findAll, findById, create };
+
+/*const tickets = [];
+
+let newId = tickets.length + 1;
+
+ Get all tickets
+function findAll() {
+  return tickets;
+}
+
+// Get ticket by ID
+function findById(id) {
+  return tickets.find(ticket => ticket.id === Number(id)) || null;
+}
+
+// Create a new ticket
+function create({ title, description, movieId, seatId }) {
+  const newTicket = {
+    id: newId++,
+    title,
+    description,
+    movieId,   // link to movie
+    seatId,    // link to seat
+    status: "open"
+  };
+  tickets.push(newTicket);
+  return newTicket;
+}
+
+// Update a ticket
+function update(id, updates) {
+  const index = tickets.findIndex(ticket => ticket.id === Number(id));
+  if (index !== -1) {
+    tickets[index] = { ...tickets[index], ...updates, id: Number(id) };
+    return tickets[index];
+  }
+  return null;
+}
+
+// Delete a ticket
+function remove(id) {
+  const index = tickets.findIndex(ticket => ticket.id === Number(id));
+  if (index !== -1) {
+    tickets.splice(index, 1);
+    return true;
+  }
+  return false;
+}
+
+module.exports = { findAll, findById, create, update, remove };*/

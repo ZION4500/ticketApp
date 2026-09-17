43d298f0d8a5e9e6a14cb75f2fad236f2ffb3398
Ivie <ivieosimen1@gmail.com>
2026-09-18 00:35:50 +0100

    The user can now add movies to their showtime and the admin can also make changes

--- backend/controller/auth.controller.js
+++
@@ -1,9 +0,0 @@
-const authService = require('../services/auth.service');
-
-function login(req, res) {
-  const { username, password } = req.body;
-  const result = authService.login(username, password);
-  res.json(result);
-}
-
-module.exports = { login };

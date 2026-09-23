5f400f8c132366cbbf15c402191b152f0a4fd004
sharon <sharonubechu@gmail.com>
2026-09-23 04:57:41 +0100

    movies done

--- frontend/src/Movies.jsx
+++ frontend/src/Movies.jsx
@@ -2,7 +2,9 @@ export default function Movies() {
   return (
     <div>
       <h1>Movies</h1>
-    
+      
+      <p>Welcome to the Movies page!</p>
+      <p>Here you can pick movies you want to watch!</p>
     </div>
   );
 }
